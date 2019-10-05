d323c887b4e37b1fe890db8a722008576f39ed69
sujanshrestha <shrestha94.sujan@gmail.com>
2019-10-05 14:36:18 -0500

    adding login page, signup page, navigation bar and some css

--- my_daily_water_app.py
+++ my_daily_water_app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
-my_daily_water_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@my_daily_water_app.route("/")
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        result = request.form
+        # TODO use the information in this reques to store the values  in database
+        return redirect('/login')
+
+    return render_template('signup.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    return render_template('login.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('login.html')
 
 
-if __name__ == '__main__': my_daily_water_app.run()
+if __name__ == '__main__': app.run()
