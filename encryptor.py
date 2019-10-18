39750f7e397f08e046b4358b84a9f16d77dc15e6
sujanshrestha <shrestha94.sujan@gmail.com>
2019-10-18 08:37:59 -0500

    javascript is not working right when buttons are clicked, so took the javascript out

---
+++ encryptor.py
@@ -0,0 +1,14 @@
+import uuid
+import hashlib
+
+
+def hash_password(password):
+    salt = uuid.uuid4().hex
+
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+
+
+def check_password_matches(hashed_password, user_password):
+    password, salt = hashed_password.split(':')
+
+    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
---
+++ static/js/main.js
@@ -0,0 +1,34 @@
+//$(function() {
+//    $('#login-button').click(function(){
+//        $.ajax({
+//            url: '/login',
+//            data: $('form').serialize(),
+//            type: 'POST',
+//            success: function(response) {
+//                console.log(response);
+//            },
+//            error: function(error) {
+//                console.log(error);
+//            }
+//        })
+//
+//    })
+//});
+
+// TODO this didn't work for me why?
+//$(function() {
+//    $('#signup-button').click(function(){
+//        $.ajax({
+//            url: '/signup',
+//            data: $('form').serialize(),
+//            type: 'POST',
+//            success: function(response) {
+//                console.log(response);
+//            },
+//            error: function(error) {
+//                console.log(error);
+//            }
+//        })
+//
+//    })
+//});
