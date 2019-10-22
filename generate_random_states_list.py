057ccfe0f72448b6b9091acfde711160c9f05c01
sujanshrestha <shrestha94.sujan@gmail.com>
2019-10-22 18:46:23 -0500

    added a python script to generate states in random order

---
+++ generate_random_states_list.py
@@ -0,0 +1,67 @@
+import random
+
+
+def generate_random_states_list():
+    statesList = [
+        'Alabama',
+        'Alaska',
+        'Arizona',
+        'Arkansas',
+        'California',
+        'Colorado',
+        'Connecticut',
+        'Delaware',
+        'Florida',
+        'Georgia',
+        'Hawaii',
+        'Idaho',
+        'Illinois',
+        'Indiana',
+        'Iowa',
+        'Kansas',
+        'Kentucky',
+        'Louisiana',
+        'Maine',
+        'Maryland',
+        'Massachusetts',
+        'Michigan',
+        'Minnesota',
+        'Mississippi',
+        'Missouri',
+        'Montana',
+        'Nebraska',
+        'Nevada',
+        'New Hampshire',
+        'New Jersey',
+        'New Mexico',
+        'New York',
+        'North Carolina',
+        'North Dakota',
+        'Ohio',
+        'Oklahoma',
+        'Oregon',
+        'Pennsylvania',
+        'Rhode Island',
+        'South Carolina',
+        'South Dakota',
+        'Tennessee',
+        'Texas',
+        'Utah',
+        'Vermont',
+        'Virginia',
+        'Washington',
+        'West Virginia',
+        'Wisconsin',
+        'Wyoming'
+    ]
+
+    for num in range(0, 50001):
+        x = random.randint(0, len(statesList)-1)
+        print(statesList[x])
+
+
+def main():
+    generate_random_states_list()
+
+
+if __name__ == '__main__': main()
