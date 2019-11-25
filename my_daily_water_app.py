3aa0661c5082070a351fb44ee282fd264c0237d8
sujanshrestha <shrestha94.sujan@gmail.com>
2019-11-25 07:04:25 -0600

    working line chart

--- my_daily_water_app.py
+++ my_daily_water_app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, json, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flaskext.mysql import MySQL
 from datetime import date
 from encryptor import *
+import simplejson as json
 
 app = Flask(__name__)
 app.secret_key = 'password'
@@ -164,18 +165,132 @@ def report(username):
     result = cursor.fetchall()
 
     if result:
-        # TODO show the result in graph form
-        print('how to show the result in graph')
-        return render_template('report.html')
+
+        # shower_data_entries = []
+        # kitchen_sink_data_entries = []
+        # bathroom_sink_data_entries = []
+        # toilet_data_entries = []
+        # drinking_water_data_entries = []
+        # sprinkler_data_entries = []
+        # miscellaneous_data_entries = []
+        # date_data_entries = []
+        water_data_result = []
+        for entry in result:
+            json_water_data_object = {
+                'shower_data': entry[1],  # using index 1 since 0 is username and am discarding it for now
+                'kitchen_sink_data': entry[2],
+                'bathroom_sink_data': entry[3],
+                'toilet_data': entry[4],
+                'drinking_water_data': entry[5],
+                'sprinkler_data': entry[6],
+                'miscellaneous_data': entry[7],
+                'date': entry[8]
+            }
+            water_data_result.append(json_water_data_object)
+
+        # json_shower_data = {
+        #     'name': 'shower_data',
+        #     'values': shower_data_entries
+        # }
+        # json_kitchen_sink_data = {
+        #     'name': 'kitchen_sink_data',
+        #     'values': kitchen_sink_data_entries
+        # }
+        # json_bathroom_sink_data = {
+        #     'name': 'shower_data',
+        #     'values': bathroom_sink_data_entries
+        # }
+        # json_toilet_data = {
+        #     'name': 'bathroom_sink_data',
+        #     'values': toilet_data_entries
+        # }
+        # json_drinking_water_data = {
+        #     'name': 'drinking_water_data',
+        #     'values': drinking_water_data_entries
+        # }
+        # json_sprinkler_data = {
+        #     'name': 'sprinkler_data',
+        #     'values': sprinkler_data_entries
+        # }
+        # json_miscellaneous_data = {
+        #     'name': 'miscellaneous_data',
+        #     'values': miscellaneous_data_entries
+        # }
+        # json_water_data = [json_shower_data, json_kitchen_sink_data, json_bathroom_sink_data,
+        #                    json_toilet_data, json_drinking_water_data, json_sprinkler_data,
+        #                    json_miscellaneous_data]
+
+        # json_water_data_object = {
+        #     'chart': 'Water data collection',
+        #     'series': json_water_data,
+        #     'dates': date_data_entries
+        # }
+
+        json_result = jsonify(water_data_result)
+        print(json_result)
+        return render_template('report.html', username=username, json_result=json_result)
+
     else:
         return json.dumps({'error': 'No record entry found for User ' + username})
+    return None
+
+
+def default_date_converter(o):
+    if isinstance(o, date):
+        return o.isoformat()
+
+
+@app.route('/ajax-state-report/<state>', defaults={'city': ''}, methods=["GET"])
+@app.route('/ajax-city-report/<city>', defaults={'state': ''}, methods=["GET"])
+def ajax_report(state, city):
+    if state:
+        get_water_usage_date_query = "SELECT D.* FROM Daily_Water_Usage AS D, Address AS A, User AS U " \
+                                     "where A.state = %s AND " \
+                                     "A.address_id = U.address_id AND " \
+                                     "U.username = D.username"
+        data = (state,)
+
+    elif city:
+        get_water_usage_date_query = "SELECT D.* FROM Daily_Water_Usage AS D, Address AS A, User AS U " \
+                                     "where A.city = %s AND " \
+                                     "A.address_id = U.address_id AND " \
+                                     "U.username = D.username"
+        data = (city,)
+
+    db_connection = mysql.connect()
+    cursor = db_connection.cursor()
+    cursor.execute(get_water_usage_date_query, data)
+    result = cursor.fetchall()
+
+    if result:
+
+        water_data_result = []
+        for entry in result:
+            json_water_data_object = {
+                'shower_data': entry[1],  # using index 1 since 0 is username and am discarding it for now
+                'kitchen_sink_data': entry[2],
+                'bathroom_sink_data': entry[3],
+                'toilet_data': entry[4],
+                'drinking_water_data': entry[5],
+                'sprinkler_data': entry[6],
+                'miscellaneous_data': entry[7],
+                'Time': entry[8]
+            }
+            water_data_result.append(json_water_data_object)
+
+        json_result = json.dumps(water_data_result, default=default_date_converter)
+        # json_result = jsonify(water_data_result)
+        print(json_result)
+        return json_result
+
+    else:
+        return json.dumps({'error': 'No record entry found'})
     return None
 
 
 @app.route('/login', defaults={'user_email': '', 'user_password': ''}, methods=['GET', 'POST'])
 @app.route('/login/<user_email>/<user_password>', methods=['GET', 'POST'])
 def login(user_email, user_password):
-
     # checks if user is already logged in, opens up the input page for that user
     if 'username' in session:
         username = session['username']
--- static/js/report.js
+++ static/js/report.js
@@ -1,79 +1,277 @@
-//var chart = $('.graph');
-//
-//var shower_data = {
-//    x: [],
-//    y: [],
-//    type: 'scatter'
-//};
-//var toilet_data = {
-//    x: [],
-//    y: [],
-//    type: 'scatter'
-//};
-//var bathroom_sink_data = {
-//    x: [],
-//    y: [],
-//    type: 'scatter'
-//};
-//var kitchen_sink_data = {
-//    x: [],
-//    y: [],
-//    type: 'scatter'
-//};
-//var drinking_water_data = {
-//    x: [],
-//    y: [],
-//    type: 'scatter'
-//};
-//var sprinkler_data = {
-//    x: [],
-//    y: [],
-//    type: 'scatter'
-//};
-//var miscellaneous_data = {
-//    x: [],
-//    y: [],
-//    type: 'scatter'
-//};
-//var date_data = {};
-//
-//var layout = {
-//  title:'A Line Chart in Plotly'
-//};
-//
-//
-//var graph_data = [shower_data, toilet_data, bathroom_sink_data, kitchen_sink_data, drinking_water_data,
-//                    sprinkler_data, miscellaneous_data];
-//
-//Plotly.plot(chart, graph_data, {});
+var json_data;
 
-
-var speedCanvas = document.getElementById("speedChart");
-
-Chart.defaults.global.defaultFontFamily = "Lato";
-Chart.defaults.global.defaultFontSize = 18;
-
-var speedData = {
-  labels: ["0s", "10s", "20s", "30s", "40s", "50s", "60s"],
-  datasets: [{
-    label: "Car Speed (mph)",
-    data: [0, 59, 75, 20, 20, 55, 40],
-  }]
-};
-
-var chartOptions = {
-  legend: {
-    display: true,
-    position: 'top',
-    labels: {
-      boxWidth: 80,
-      fontColor: 'black'
-    }
-  }
-};
-
-var lineChart = new Chart(speedCanvas, {
-  type: 'line',
-  data: speedData,
-  options: chartOptions
+$(document).ready(function() {
+    $('.list').hide();
+    $('#get_chart').click(function() {
+        var selected = $('#graph').val();
+        var selected_chart = '';
+        if (selected == 'State') {
+            selected_chart = $('#inputState').val();
+            url = '/ajax-state-report/' + selected_chart;
+        } else {
+            selected_chart = $('#inputCity').val();
+            url = '/ajax-city-report/' + selected_chart;
+        }
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'JSON',
+            success: function(response) {
+                generate_graph(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
 });
+
+$(document).change(function() {
+    $('.state').hide();
+    $('.city').hide();
+    var selected = $('#graph').val();
+    if (selected == 'State') {
+        $('.state').show();
+    } else {
+        $('.city').show();
+    }
+});
+
+
+
+//Read the data
+function generate_graph(data) {
+
+    //Clear the svg before loading
+    d3.selectAll('svg > g > *').remove();
+    // Draw a line chart
+    var svg = d3.select('svg'),
+      margin = { top: 20, right: 50, bottom: 30, left: 50 },
+      width = +svg.attr('width') - margin.left - margin.right,
+      height = +svg.attr('height') - margin.top - margin.bottom,
+      g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    // Graph title
+    g.append('text')
+      .attr('x', (width / 2))
+      .attr('y', 0 - (margin.top / 3))
+      .attr('text-anchor', 'middle')
+      .style('font-size', '16px')
+      .text('Water usage chart');
+    // Function to convert a string into a time
+    var parseTime = d3.time.format('%Y-%m-%d').parse;
+    // Function to show specific time format
+    var formatTime = d3.time.format('%e %B');
+
+    // Set the X scale
+    var x = d3.time.scale().range([0, width], 0.5);
+    // Set the Y scale
+    var y = d3.scale.linear().range([height, 0]);
+    // Set the color scale
+    var color = d3.scale.category10();
+
+    var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+
+    function sortByDates(a, b) {
+        return new Date(a.Time) - new Date(b.Time);
+    }
+    data = data.sort(sortByDates);
+
+      // load the data
+    {
+      // Select the important columns
+      color.domain(d3.keys(data[7]).filter(function(key) {
+          return key !== "Time" && key !== "_id";
+      }));
+      // Correct the types
+      data.forEach(function(d) {
+        console.log(d.Time);
+        d.date = parseTime(d.Time);
+        console.log("second "+d.date);
+      });
+      console.log(data);
+
+      var water_usages = color.domain().map(function(name) {
+        return {
+          name: name,
+          values: data.map(function(d) {
+            return {
+              date: d.date,
+              worth: +d[name]
+            };
+          })
+        };
+      });
+      console.log(water_usages)
+      // Set the X domain
+      x.domain(d3.extent(data, function(d) {
+        return d.date;
+      }));
+
+    var line = d3.svg.line()
+    // .interpolate("basis")
+    .x(function(d) {
+      return x(d.date);
+    })
+    .y(function(d) {
+      return y(d.worth);
+    });
+
+      // Set the Y domain
+      y.domain([
+        d3.min(water_usages, function(c) {
+          return d3.min(c.values, function(v) {
+            return v.worth;
+          });
+        }),
+        d3.max(water_usages, function(c) {
+          return d3.max(c.values, function(v) {
+            return v.worth;
+          });
+        })
+      ]);
+      // Set the X axis
+      g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+      // Set the Y axis
+      g.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Water Usage (Gal)");
+
+      // Draw the lines
+      var water_usage = g.selectAll(".water_usage")
+        .data(water_usages)
+        .enter().append("g")
+        .attr("class", "water_usage");
+
+      water_usage.append("path")
+        .attr("class", "line")
+        .attr("d", function(d) {
+          return line(d.values);
+        })
+        .style("stroke", function(d) {
+          return color(d.name);
+        });
+      // Add the circles
+      water_usage.append("g").selectAll("circle")
+        .data(function(d){return d.values})
+        .enter()
+        .append("circle")
+        .attr("r", 2)
+        .attr("cx", function(dd){return x(dd.date)})
+        .attr("cy", function(dd){return y(dd.worth)})
+        .attr("fill", "none")
+        .attr("stroke", function(d){return color(this.parentNode.__data__.name)});
+      // Add label to the end of the line
+      water_usage.append("text")
+        .attr("class", "label")
+        .datum(function (d) {
+          return {
+            name: d.name,
+            value: d.values[d.values.length - 1]
+          };
+        })
+        .attr("transform", function (d) {
+          return "translate(" + x(d.value.date) + "," + y(d.value.worth) + ")";
+        })
+        .attr("x", 3)
+        .attr("dy", ".35em")
+        .text(function (d) {
+          return d.name;
+      });
+
+    // Add the mouse line
+    var mouseG = g.append("g")
+      .attr("class", "mouse-over-effects");
+
+    mouseG.append("path")
+      .attr("class", "mouse-line")
+      .style("stroke", "black")
+      .style("stroke-width", "1px")
+      .style("opacity", "0");
+
+    var lines = document.getElementsByClassName('line');
+
+    var mousePerLine = mouseG.selectAll('.mouse-per-line')
+      .data(water_usages)
+      .enter()
+      .append("g")
+      .attr("class", "mouse-per-line");
+
+    mousePerLine.append("circle")
+      .attr("r", 7)
+      .style("stroke", function (d) {
+        return color(d.name);
+      })
+      .style("fill", "none")
+      .style("stroke-width", "2px")
+      .style("opacity", "0");
+
+    mousePerLine.append("text")
+        .attr("class", "hover-text")
+        .attr("dy", "-1em")
+        .attr("transform", "translate(10,3)");
+
+    // Append a rect to catch mouse movements on canvas
+    mouseG.append('svg:rect')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('fill', 'none')
+      .attr('pointer-events', 'all')
+      .on('mouseout', function () { // on mouse out hide line, circles and text
+        d3.select(".mouse-line")
+          .style("opacity", "0");
+        d3.selectAll(".mouse-per-line circle")
+          .style("opacity", "0");
+        d3.selectAll(".mouse-per-line text")
+          .style("opacity", "0");
+      })
+      .on('mouseover', function () { // on mouse in show line, circles and text
+        d3.select(".mouse-line")
+          .style("opacity", "1");
+        d3.selectAll(".mouse-per-line circle")
+          .style("opacity", "1");
+        d3.selectAll(".mouse-per-line text")
+          .style("opacity", "1");
+      })
+      .on('mousemove', function () { // mouse moving over canvas
+        var mouse = d3.mouse(this);
+
+        d3.selectAll(".mouse-per-line")
+          .attr("transform", function (d, i) {
+
+            var xDate = x.invert(mouse[0]),
+              bisect = d3.bisector(function (d) { return d.date; }).left;
+            idx = bisect(d.values, xDate);
+
+            d3.select(this).select('text')
+              .text(y.invert(y(d.values[idx].worth)).toFixed(2));
+
+            d3.select(".mouse-line")
+              .attr("d", function () {
+                var data = "M" + x(d.values[idx].date) + "," + height;
+                data += " " + x(d.values[idx].date) + "," + 0;
+                return data;
+              });
+            return "translate(" + x(d.values[idx].date) + "," + y(d.values[idx].worth) + ")";
+          });
+      });
+
+
+    };
+
+}
