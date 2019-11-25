414e4fb0f3920c6348d75ec6b7467b9a1e2ccd0f
sujanshrestha <shrestha94.sujan@gmail.com>
2019-11-25 16:29:25 -0600

    working on the multiline chart

--- static/js/report.js
+++ static/js/report.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
             type: 'GET',
             dataType: 'JSON',
             success: function(response) {
+            console.log(response)
                 generate_graph(response);
             },
             error: function(error) {
@@ -44,234 +45,111 @@ function generate_graph(data) {
 
     //Clear the svg before loading
     d3.selectAll('svg > g > *').remove();
-    // Draw a line chart
-    var svg = d3.select('svg'),
-      margin = { top: 20, right: 50, bottom: 30, left: 50 },
-      width = +svg.attr('width') - margin.left - margin.right,
-      height = +svg.attr('height') - margin.top - margin.bottom,
-      g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-    // Graph title
-    g.append('text')
-      .attr('x', (width / 2))
-      .attr('y', 0 - (margin.top / 3))
-      .attr('text-anchor', 'middle')
-      .style('font-size', '16px')
-      .text('Water usage chart');
-    // Function to convert a string into a time
-    var parseTime = d3.time.format('%Y-%m-%d').parse;
-    // Function to show specific time format
-    var formatTime = d3.time.format('%e %B');
 
-    // Set the X scale
-    var x = d3.time.scale().range([0, width], 0.5);
-    // Set the Y scale
-    var y = d3.scale.linear().range([height, 0]);
-    // Set the color scale
-    var color = d3.scale.category10();
+    height = 600
+    width = 1000
+    margin = ({top: 20, right: 20, bottom: 30, left: 30})
 
-    var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+    x = d3.scaleUtc()
+       .domain(d3.extent(data.dates))
+       .range([margin.left, width - margin.right])
 
-    var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+    y = d3.scaleLinear()
+       .domain([0, d3.max(data.series, d => d3.max(d.values))]).nice()
+       .range([height - margin.bottom, margin.top])
 
 
-    function sortByDates(a, b) {
-        return new Date(a.Time) - new Date(b.Time);
-    }
-    data = data.sort(sortByDates);
+    xAxis = g => g
+        .attr("transform", `translate(0,${height - margin.bottom})`)
+        .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
 
-      // load the data
-    {
-      // Select the important columns
-      color.domain(d3.keys(data[7]).filter(function(key) {
-          return key !== "Time" && key !== "_id";
-      }));
-      // Correct the types
-      data.forEach(function(d) {
-        console.log(d.Time);
-        d.date = parseTime(d.Time);
-        console.log("second "+d.date);
-      });
-      console.log(data);
+    yAxis = g => g
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(d3.axisLeft(y))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.select(".tick:last-of-type text").clone()
+            .attr("x", 3)
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .text(data.y))
 
-      var water_usages = color.domain().map(function(name) {
-        return {
-          name: name,
-          values: data.map(function(d) {
-            return {
-              date: d.date,
-              worth: +d[name]
-            };
-          })
-        };
-      });
-      console.log(water_usages)
-      // Set the X domain
-      x.domain(d3.extent(data, function(d) {
-        return d.date;
-      }));
 
-    var line = d3.svg.line()
-    // .interpolate("basis")
-    .x(function(d) {
-      return x(d.date);
-    })
-    .y(function(d) {
-      return y(d.worth);
-    });
+    line = d3.line()
+        .defined(d => !isNaN(d))
+        .x(d => x(d))
+//        .x((d, i) => x(data.dates[i]))
+        .y(d => y(d))
 
-      // Set the Y domain
-      y.domain([
-        d3.min(water_usages, function(c) {
-          return d3.min(c.values, function(v) {
-            return v.worth;
-          });
-        }),
-        d3.max(water_usages, function(c) {
-          return d3.max(c.values, function(v) {
-            return v.worth;
-          });
-        })
-      ]);
-      // Set the X axis
-      g.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
+    const svg = d3.select("svg")
+        .attr("viewBox", [0, 0, width, height]);
+
+    svg.append("g")
         .call(xAxis);
-      // Set the Y axis
-      g.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Water Usage (Gal)");
 
-      // Draw the lines
-      var water_usage = g.selectAll(".water_usage")
-        .data(water_usages)
-        .enter().append("g")
-        .attr("class", "water_usage");
+    svg.append("g")
+        .call(yAxis);
 
-      water_usage.append("path")
-        .attr("class", "line")
-        .attr("d", function(d) {
-          return line(d.values);
-        })
-        .style("stroke", function(d) {
-          return color(d.name);
-        });
-      // Add the circles
-      water_usage.append("g").selectAll("circle")
-        .data(function(d){return d.values})
-        .enter()
-        .append("circle")
-        .attr("r", 2)
-        .attr("cx", function(dd){return x(dd.date)})
-        .attr("cy", function(dd){return y(dd.worth)})
+    const path = svg.append("g")
         .attr("fill", "none")
-        .attr("stroke", function(d){return color(this.parentNode.__data__.name)});
-      // Add label to the end of the line
-      water_usage.append("text")
-        .attr("class", "label")
-        .datum(function (d) {
-          return {
-            name: d.name,
-            value: d.values[d.values.length - 1]
-          };
-        })
-        .attr("transform", function (d) {
-          return "translate(" + x(d.value.date) + "," + y(d.value.worth) + ")";
-        })
-        .attr("x", 3)
-        .attr("dy", ".35em")
-        .text(function (d) {
-          return d.name;
-      });
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+      .selectAll("path")
+      .data(data.series)
+      .join("path")
+        .style("mix-blend-mode", "multiply")
+        .attr("d", d => line(d.values));
 
-    // Add the mouse line
-    var mouseG = g.append("g")
-      .attr("class", "mouse-over-effects");
+    svg.call(hover, path);
 
-    mouseG.append("path")
-      .attr("class", "mouse-line")
-      .style("stroke", "black")
-      .style("stroke-width", "1px")
-      .style("opacity", "0");
+    function hover(svg, path) {
+      svg.style("position", "relative");
 
-    var lines = document.getElementsByClassName('line');
+      if ("ontouchstart" in document) svg
+          .style("-webkit-tap-highlight-color", "transparent")
+          .on("touchmove", moved)
+          .on("touchstart", entered)
+          .on("touchend", left)
+      else svg
+          .on("mousemove", moved)
+          .on("mouseenter", entered)
+          .on("mouseleave", left);
 
-    var mousePerLine = mouseG.selectAll('.mouse-per-line')
-      .data(water_usages)
-      .enter()
-      .append("g")
-      .attr("class", "mouse-per-line");
+      const dot = svg.append("g")
+          .attr("display", "none");
 
-    mousePerLine.append("circle")
-      .attr("r", 7)
-      .style("stroke", function (d) {
-        return color(d.name);
-      })
-      .style("fill", "none")
-      .style("stroke-width", "2px")
-      .style("opacity", "0");
+      dot.append("circle")
+          .attr("r", 2.5);
 
-    mousePerLine.append("text")
-        .attr("class", "hover-text")
-        .attr("dy", "-1em")
-        .attr("transform", "translate(10,3)");
+      dot.append("text")
+          .style("font", "10px sans-serif")
+          .attr("text-anchor", "middle")
+          .attr("y", -8);
 
-    // Append a rect to catch mouse movements on canvas
-    mouseG.append('svg:rect')
-      .attr('width', width)
-      .attr('height', height)
-      .attr('fill', 'none')
-      .attr('pointer-events', 'all')
-      .on('mouseout', function () { // on mouse out hide line, circles and text
-        d3.select(".mouse-line")
-          .style("opacity", "0");
-        d3.selectAll(".mouse-per-line circle")
-          .style("opacity", "0");
-        d3.selectAll(".mouse-per-line text")
-          .style("opacity", "0");
-      })
-      .on('mouseover', function () { // on mouse in show line, circles and text
-        d3.select(".mouse-line")
-          .style("opacity", "1");
-        d3.selectAll(".mouse-per-line circle")
-          .style("opacity", "1");
-        d3.selectAll(".mouse-per-line text")
-          .style("opacity", "1");
-      })
-      .on('mousemove', function () { // mouse moving over canvas
-        var mouse = d3.mouse(this);
+      function moved() {
+        d3.event.preventDefault();
+        const ym = y.invert(d3.event.layerY);
+        const xm = x.invert(d3.event.layerX);
+        const i1 = d3.bisectLeft(data.dates, xm, 1);
+        const i0 = i1 - 1;
+        const i = xm - data.dates[i0] > data.dates[i1] - xm ? i1 : i0;
+        const s = data.series.reduce((a, b) => Math.abs(a.values[i] - ym) < Math.abs(b.values[i] - ym) ? a : b);
+        path.attr("stroke", d => d === s ? null : "#ddd").filter(d => d === s).raise();
+        dot.attr("transform", `translate(${x(data.dates[i])},${y(s.values[i])})`);
+        dot.select("text").text(s.name);
+      }
 
-        d3.selectAll(".mouse-per-line")
-          .attr("transform", function (d, i) {
+      function entered() {
+        path.style("mix-blend-mode", null).attr("stroke", "#ddd");
+        dot.attr("display", null);
+      }
 
-            var xDate = x.invert(mouse[0]),
-              bisect = d3.bisector(function (d) { return d.date; }).left;
-            idx = bisect(d.values, xDate);
+      function left() {
+        path.style("mix-blend-mode", "multiply").attr("stroke", null);
+        dot.attr("display", "none");
+      }
+    }
 
-            d3.select(this).select('text')
-              .text(y.invert(y(d.values[idx].worth)).toFixed(2));
-
-            d3.select(".mouse-line")
-              .attr("d", function () {
-                var data = "M" + x(d.values[idx].date) + "," + height;
-                data += " " + x(d.values[idx].date) + "," + 0;
-                return data;
-              });
-            return "translate(" + x(d.values[idx].date) + "," + y(d.values[idx].worth) + ")";
-          });
-      });
-
-
-    };
-
+    return svg.node();
 }
